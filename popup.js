ea76af579ac842f26e7f02371078634e9bc0e321
blobsey <blobs03@gmail.com>
2024-03-09 19:15:13 -0600

    added status indicator + fixed bug with pasting large amont sof text

--- popup.js
+++ popup.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 const menus = {
     "config": {
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="white" stroke="white">
         <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65c-.05-.23-.25-.4-.49-.4h-4c-.24 0-.44.17-.49.4l-.38 2.65c-.61.25-1.17.58-1.69.98l-2.49-1c-.22-.08-.49 0-.61.22l-2 3.46c-.12.22-.07.49.12.64l2.11 1.65c-.04.32-.07.64-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.05.23.25.4.49.4h4c.24 0 .44-.17.49-.4l.38-2.65c.61-.25 1.17-.58 1.69-.98l2.49 1c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"/>
         </svg>`,
         func: createConfigScreen
@@ -41,6 +41,17 @@ const menus = {
         func: createAddScreen
     }
 }
+
+const loadingSvg = `<svg width="24" height="24" viewBox="0 0 24 24" stroke="white" fill="white" xmlns="http://www.w3.org/2000/svg"><style>.spinner_I8Q1{animation:spinner_qhi1 .75s linear infinite}.spinner_vrS7{animation-delay:-.375s}@keyframes spinner_qhi1{0%,100%{r:1.5px}50%{r:3px}}</style><circle class="spinner_I8Q1" cx="4" cy="12" r="1.5"/><circle class="spinner_I8Q1 spinner_vrS7" cx="12" cy="12" r="3"/><circle class="spinner_I8Q1" cx="20" cy="12" r="1.5"/><script xmlns="" id="bw-fido2-page-script"/>
+</svg>`;
+
+const successSvg = `<svg viewBox="0 0 24 24" width="24" height="24" fill="none" !important xmlns="http://www.w3.org/2000/svg">
+<path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#FFFFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+
+const errorSvg = `<svg width="24" height="24" viewBox="-4 -4 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024">
+<path d="M7.493 0.015 C 7.442 0.021,7.268 0.039,7.107 0.055 C 5.234 0.242,3.347 1.208,2.071 2.634 C 0.660 4.211,-0.057 6.168,0.009 8.253 C 0.124 11.854,2.599 14.903,6.110 15.771 C 8.169 16.280,10.433 15.917,12.227 14.791 C 14.017 13.666,15.270 11.933,15.771 9.887 C 15.943 9.186,15.983 8.829,15.983 8.000 C 15.983 7.171,15.943 6.814,15.771 6.113 C 14.979 2.878,12.315 0.498,9.000 0.064 C 8.716 0.027,7.683 -0.006,7.493 0.015 M8.853 1.563 C 9.967 1.707,11.010 2.136,11.944 2.834 C 12.273 3.080,12.920 3.727,13.166 4.056 C 13.727 4.807,14.142 5.690,14.330 6.535 C 14.544 7.500,14.544 8.500,14.330 9.465 C 13.916 11.326,12.605 12.978,10.867 13.828 C 10.239 14.135,9.591 14.336,8.880 14.444 C 8.456 14.509,7.544 14.509,7.120 14.444 C 5.172 14.148,3.528 13.085,2.493 11.451 C 2.279 11.114,1.999 10.526,1.859 10.119 C 1.618 9.422,1.514 8.781,1.514 8.000 C 1.514 6.961,1.715 6.075,2.160 5.160 C 2.500 4.462,2.846 3.980,3.413 3.413 C 3.980 2.846,4.462 2.500,5.160 2.160 C 6.313 1.599,7.567 1.397,8.853 1.563 M7.706 4.290 C 7.482 4.363,7.355 4.491,7.293 4.705 C 7.257 4.827,7.253 5.106,7.259 6.816 C 7.267 8.786,7.267 8.787,7.325 8.896 C 7.398 9.033,7.538 9.157,7.671 9.204 C 7.803 9.250,8.197 9.250,8.329 9.204 C 8.462 9.157,8.602 9.033,8.675 8.896 C 8.733 8.787,8.733 8.786,8.741 6.816 C 8.749 4.664,8.749 4.662,8.596 4.481 C 8.472 4.333,8.339 4.284,8.040 4.276 C 7.893 4.272,7.743 4.278,7.706 4.290 M7.786 10.530 C 7.597 10.592,7.410 10.753,7.319 10.932 C 7.249 11.072,7.237 11.325,7.294 11.495 C 7.388 11.780,7.697 12.000,8.000 12.000 C 8.303 12.000,8.612 11.780,8.706 11.495 C 8.763 11.325,8.751 11.072,8.681 10.932 C 8.616 10.804,8.460 10.646,8.333 10.580 C 8.217 10.520,7.904 10.491,7.786 10.530 " stroke="none" fill-rule="evenodd" fill="#ffffff"/>
+</svg>`;
 
 async function createConfigScreen() {
     const contentDiv = document.getElementById('content');
@@ -111,6 +122,10 @@ async function createConfigScreen() {
 
 // Helper function to grow a textarea based on content
 function adjustSize(textarea) {
+    // Adjust height
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+
     // Ensure measuringSpan is created and styled
     let measuringSpan = document.getElementById('measuringSpan');
     if (!measuringSpan) {
@@ -119,6 +134,8 @@ function adjustSize(textarea) {
         measuringSpan.style.visibility = 'hidden';
         measuringSpan.style.position = 'absolute';
         measuringSpan.style.whiteSpace = 'pre';
+        // Jank to ensure that measuringSpan doesn't get too tall
+        measuringSpan.style.transform = 'translateY(-10000px)'; 
         document.body.appendChild(measuringSpan);
     }
 
@@ -129,11 +146,7 @@ function adjustSize(textarea) {
     measuringSpan.textContent = textarea.value || textarea.placeholder;
 
     // Adjust width based on measuringSpan - ensure no maxWidth constraint
-    textarea.style.width = `${measuringSpan.offsetWidth + 20}px`; // Additional padding if necessary
-
-    // Adjust height
-    textarea.style.height = 'auto';
-    textarea.style.height = `${textarea.scrollHeight}px`;
+    textarea.style.width = `${measuringSpan.offsetWidth + 30}px`; 
 }
 
 async function createAddScreen() {
@@ -175,17 +188,29 @@ async function createAddScreen() {
     const submitButton = document.createElement('button');
     submitButton.textContent = 'Add Flashcard';
 
+    const statusDiv = document.createElement('div');
+    statusDiv.id = "statusIndicator";
+
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.id = "buttonsDiv";
+    buttonsDiv.appendChild(submitButton);
+    buttonsDiv.appendChild(statusDiv);
+    
+
     // Append elements to the form
     form.appendChild(textareaFront);
     form.appendChild(inputBack);
-    form.appendChild(submitButton);
+    form.appendChild(buttonsDiv);
     contentDiv.appendChild(form);
 
     // Add event listener for the form submission
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
+        submitButton.disabled = true;
         const cardFront = textareaFront.value;
         const cardBack = inputBack.value;
+
+        statusDiv.innerHTML = loadingSvg;
 
         // Send message to background.js to add the flashcard
         try {
@@ -194,13 +219,23 @@ async function createAddScreen() {
                 card_front: cardFront,
                 card_back: cardBack
             });
-
-            // Remove cached input
-            browser.storage.local.remove(['cardFrontInput', 'cardBackInput']);
-            console.log(response); // Log the response from background.js
-            window.close();
+            
+            switch(response.result) {
+                case "success":
+                    statusDiv.innerHTML = successSvg;
+                    statusDiv.title = "";
+                    break;
+                default:
+                    statusDiv.innerHTML = errorSvg;
+                    statusDiv.title = response.message;
+                    throw new Error(response.message);
+                    break;
+            }
         } catch (error) {
+            statusDiv.innerHTML = errorSvg;
             console.error('Error adding flashcard:', error);
+        } finally {
+            submitButton.disabled = false;
         }
     });
 }
