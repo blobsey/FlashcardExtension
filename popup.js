cc797b7994dc85b8c43fa20bd9760e7c9cd3155a
blobsey <blobs03@gmail.com>
2024-06-07 23:40:11 -0500

    added a pop-out button to 'add' screen

--- popup.js
+++ popup.js
@@ -42,9 +42,7 @@ async function updateNavbar() {
     if (history.length > 1) {
         const backButton = document.createElement('button');
         backButton.id = "backButton";
-        backButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" fill="#fff" stroke="#fff" stroke-width="34.816" class="icon" viewBox="0 0 1024 1024">
-        <path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-.8 88.8l309.6 280z"/>
-        </svg>`;
+        loadSvg(backButton, 'back');
         backButton.addEventListener('click', () => {
             history.pop();
             menuOptions[history[history.length - 1]].func();
@@ -571,6 +569,21 @@ async function createAddScreen() {
     });
 
     applyPlaceholderStyle(); // If selected option is "" then apply placeholder style (make less opaque)
+
+    // 'Pop out' button
+    const popOutButton = document.createElement('button');
+    popOutButton.id = 'pop-out-button';
+    loadSvg(popOutButton, 'popout');
+    popOutButton.addEventListener('click', async (event) => {
+        const popup = await browser.windows.create({
+            url:  browser.runtime.getURL(`blank.html?screenToLoad=add`),
+            type: 'popup',
+            width: 500,
+            height: 600
+        });
+    });
+    contentDiv.appendChild(popOutButton);
+
 
     // Listeners to update local storage whenever user types
     textareaFront.addEventListener('input', function() {
